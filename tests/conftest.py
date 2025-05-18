6e0acb8b9f9a50dc1b22432d60506c4a471d277b
royy29 <80411291+royy29@users.noreply.github.com>
2025-05-19 01:17:34 +0530

    implemented pytest for rentals testing- update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,3 @@
-# import pytest
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from app.models import Base
-# from app.main import app
-# from fastapi.testclient import TestClient
-
-# TEST_DATABASE_URL = "sqlite:///./test_rental_manager.db"
-
-# engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
-# TestingSessionLocal = sessionmaker(bind=engine)
-
-# # Override get_db
-# def override_get_db():
-#     db = TestingSessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-# @pytest.fixture(scope="module")
-# def client():
-#     # Recreate test database each time
-#     Base.metadata.drop_all(bind=engine)
-#     Base.metadata.create_all(bind=engine)
-
-#     from app.routers import rentals
-#     app.dependency_overrides[rentals.get_db] = override_get_db
-
-#     with TestClient(app) as c:
-#         yield c
 
 
 import pytest
